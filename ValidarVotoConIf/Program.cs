c0687cd1c7b18f7fb51ac95a7d8b1997ce0bc66f
tabatamelisa <tabatavernaza12@gmail.com>
2025-03-13 13:30:04 -0500

    primer commit

---
+++ ValidarVotoConIf/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidarVotoConIf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verificar si una persona puede votar usando if-else
+            //Andy Garzón
+            int edad;
+            do
+            {
+                Console.Write("Introduce tu edad para verificar si puedes votar: ");
+                edad = int.Parse(Console.ReadLine());
+
+                if (edad >= 18)
+                {
+                    Console.WriteLine("Si puedes votar.");
+                }
+                else
+                {
+                    Console.WriteLine("No puedes votar.");
+                }
+            } while (true); //El programa se repite
+        }
+    }
+}
